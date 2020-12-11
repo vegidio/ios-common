0c0129708eec453f1bad9f22c74ef87bb9d07d87
Vinicius Egidio <me@vinicius.io>
2020-12-11 21:23:22 +0100

    Initial commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  iOS Common
+//
+//  Created by Vinícius Egidio on 2020-12-11.
+//  Copyright © 2020 vinicius.io - All rights reserved.
+//
+
+import SwiftUI
+
+internal struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+private struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/MainApp.swift
@@ -0,0 +1,18 @@
+//
+//  MainApp.swift
+//  iOS Common
+//
+//  Created by Vinícius Egidio on 2020-12-11.
+//  Copyright © 2020 vinicius.io - All rights reserved.
+//
+
+import SwiftUI
+
+@main
+internal struct MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

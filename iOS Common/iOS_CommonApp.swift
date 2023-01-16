777b5d57f727648c475e26f88bdd0089a974d336
Vinicius Egidio <me@vinicius.io>
2023-01-16 10:31:19 +0100

    Initial Commit

---
+++ iOS Common/iOS_CommonApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_CommonApp.swift
+//  iOS Common
+//
+//  Created by Vinicius Egidio on 2023-01-16.
+//
+
+import SwiftUI
+
+@main
+struct iOS_CommonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
